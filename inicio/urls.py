1a8ec5026a405f6cae11337f6322e8733db544e3
mdoliveira1988 <mdoliveira1988@bol.com.br>
2023-04-07 23:53:37 -0300

    Ajuste na index

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from inicio.views import index
+from SaibaMais import views
+from inicio.views import index, my_api_view
 
 urlpatterns = [
     path('', index),
+    path('my-api/', my_api_view, name='my_api'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,14 @@
+from django.contrib.sites import requests
+from django.http import JsonResponse
 from django.shortcuts import render
+import requests
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def my_api_view(request):
+    response = requests.get('https://exemplo.com/api/')
+    data = response.json()
+    return JsonResponse(data)
